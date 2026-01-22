5c3ff8a2a0599f8be0644ce48d911deb05389ab2
Adin Smith <adins1761@gmail.com>
2026-01-22 10:45:14 -0800

    Added 2D physics to the player

--- character.js
+++ character.js
@@ -3,34 +3,76 @@ let state = 0;
 class Character {
     constructor(game) {
         this.game = game;
-        this.animators = [];
-        this.x = 0;
+        this.x = 50;
         this.y = 0;
         this.speed = 400;
+        this.facingleft = false;
+        this.velocity = { x : 0, y : 0 };
+        this.fallAcc = 562.5;
 
-        for (const f of files) {
-            const sprite = ASSET_MANAGER.getAsset(`./sprites/Warrior_${f}.png`);
-            this.animators.push(new Animator(sprite, 0, 0, 192, 192, 4, 0.15));
-        }
+        // for (const f of files) {
+        //     const sprite = ASSET_MANAGER.getAsset(`./sprites/Warrior_${f}.png`);
+        //     this.animators.push(new Animator(sprite, 0, 0, 192, 192, 4, 0.15));
+        // }
+
+        const sprite = ASSET_MANAGER.getAsset("./sprites/Astronaut_Player.png");
+        this.animators = {
+            "idle": new Animator(sprite, 7, 65, 32, 35, 1, 0.15),
+            "walk": new Animator(sprite, 7, 65, 32, 35, 8, 0.15),
+            "jump": new Animator(sprite, 7, 65, 32, 35, 1, 0.15)
+        };
     };
 
     update() {
-        state = 0
+        state = "idle"
         const keys = this.game.keys;
-        const d = this.speed * this.game.clockTick;
+        const TICK = this.game.clockTick;
+        const d = this.speed * TICK;
 
-        if (keys["w"] || keys["ArrowUp"]) { this.y -= d; state = 1; }
-        if (keys["s"] || keys["ArrowDown"]) { this.y += d; state = 1; }
-        if (keys["a"] || keys["ArrowLeft"]) { this.x -= d; state = 1; }
-        if (keys["d"] || keys["ArrowRight"]) { this.x += d; state = 1; }
+        if (keys["a"] || keys["ArrowLeft"]) { 
+            this.x -= d; 
+            state = "walk"; 
+            this.facingleft = true; 
+        }
+        if (keys["d"] || keys["ArrowRight"]) { 
+            this.x += d; 
+            state = "walk"; 
+            this.facingleft = false; 
+        }
 
-        if (this.x > 900) this.x = -300;
-        if (this.x < -300) this.x = 900;
-        if (this.y > 650) this.y = -300;
-        if (this.y < -300) this.y = 650;
+        const groundY = 500;
+        const onGround = this.y >= groundY;
+        if ((keys["w"] || keys["ArrowUp"]) && onGround) { 
+            
+            state = "jump"; 
+            this.velocity.y = -550;
+        }
+
+        // gravity physics 
+        this.velocity.y += this.fallAcc * TICK;
+        this.y += this.velocity.y * TICK;
+
+        if (this.y > groundY) {
+            this.y = groundY;
+            this.velocity.y = 0;
+        }
+
+        if (!onGround) {
+            state = "jump"
+        }
+
+        if (this.x > 1024) this.x = 0;
+        if (this.x < 0) this.x = 1024;
     };
 
     draw(ctx) {
-        this.animators[state].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        if (this.facingleft) {
+            ctx.save();
+            ctx.scale(-1, 1);
+            this.animators[state].drawFrame(this.game.clockTick, ctx, -(this.x) - 50, this.y);
+            ctx.restore();
+        } else {
+            this.animators[state].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        }
     };
 }
--- main.js
+++ main.js
@@ -2,17 +2,19 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-const files = [
-	"Idle",
-	"Run",
-	"Attack1",
-	"Attack2",
-	"Guard",
-];
+// const files = [
+// 	"Idle",
+// 	"Run",
+// 	"Attack1",
+// 	"Attack2",
+// 	"Guard",
+// ];
 
-for (const f of files) {
-	ASSET_MANAGER.queueDownload(`./sprites/Warrior_${f}.png`);
-}
+// for (const f of files) {
+// 	ASSET_MANAGER.queueDownload(`./sprites/Warrior_${f}.png`);
+// }
+
+ASSET_MANAGER.queueDownload("./sprites/Astronaut_Player.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
